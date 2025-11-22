666a3429ef4159017de7dfe6e96a36627c2a0d58
TechCat <a.m.techcat@gmail.com>
2025-11-22 15:04:35 +0900

    Initial commit: set up Django project with backend and tasks app

---
+++ backend/tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# 例: Task モデル
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
---
+++ backend/tasks/views.py
@@ -0,0 +1,9 @@
+# tasks/views.py
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskListView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
