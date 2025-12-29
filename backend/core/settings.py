54b920d7900031adaad054c5f19aa1a95da5ba8d
TechCat <a.m.techcat@gmail.com>
2025-12-29 15:43:14 +0900

    Add CSRF_TRUSTED_ORIGINS setting for production

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -179,3 +179,7 @@ if os.getenv('CORS_ALLOWED_ORIGINS'):
 else:
     CORS_ALLOW_ALL_ORIGINS = True
 
+# CSRF Settings for production
+if os.getenv('CSRF_TRUSTED_ORIGINS'):
+    CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
+
